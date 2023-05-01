a8be3a54a5461c6b48213fdefcd1c7384970bc96
mburgosc <mburgosc@esliceu.net>
2023-05-01 14:42:49 +0200

    dev: added motor-scraper and env variable selector

--- src/fuel-prices.motor.scraper.js
+++ src/fuel-prices.motor.scraper.js
@@ -3,19 +3,42 @@ const puppeteer = require('puppeteer')
 const url = 'https://www.motor.es/energia/precios-combustible-hoy'
 
 async function getResults(filters) {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+    })
+    await page.goto(url)
+    // click accept coockies so the pop up dissapears
+    page.waitForSelector("#denegar_btn")
+    page.click("#denegar_btn")
 
-    
-        const result = (await page.evaluate(() => document.querySelector("td.sd").innerHTML)).split(">")[1]
-        results.push({
-            headers: [filters[0].name, filters[1].name, filters[2].name, filters[3].name],
-            params: [filters[0].values[i1], filters[1].values[4], filters[2].values[i3], filters[3].values[i4]],
-            result: parseInt(result).toFixed(1)
-        })
+    // remove the blocking div
+    await page.evaluate(() => {
+        var element = document.querySelector("sibbo-cmp-layout");
+        element.parentNode.removeChild(element);
+    })
+
+    await page.waitForTimeout(1000)
+
+    await page.focus("table.resultados-table")
+    const results = []
+    const fuelTypes = ['Gasolina 95', 'Gasolina 98', 'Gasóleo A', 'Gasóleo A+', 'Gasóleo B']
+    for (let i = 1; i < 6; i++) {
+        const result = await page.evaluate((i) => {
+            window.scrollTo(0, document.body.scrollHeight / 2);
+            const div = document.querySelector(".resultados-table-wrapper")
+            div.scrollBy(0, div.scrollHeight);
+            const secondTable = document.querySelectorAll(`table.resultados-table`)[1]
+            return secondTable.querySelector(`tbody tr:nth-child(${i}) td:nth-child(10) span`).innerHTML.replace(",",".")
+        }, i)
         console.log(result)
-
-        
-        await page.goBack()
-        await page.waitForTimeout(1000)
+        results.push({
+            fuelType: fuelTypes[i-1],
+            result: parseFloat(result)
+        })
     }
     browser.close()
     return results
@@ -23,10 +46,9 @@ async function getResults(filters) {
 }
 
 const scrap = async () => {
-    const filters = await setFilters()
-    return await getResults(filters)
+    return await getResults()
 }
 
 module.exports = {
-  scrap
+    scrap
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const ineScraper = require('./urban-travels.ine.scraper')
+const fuelScraper = require('./fuel-prices.motor.scraper')
 const formmater = require('./jsonDataFormatter')
 
 // run scrapers
-
-async function start(params) {
-  const data = await ineScraper.scrap();
-  formmater.formatData(data);
+async function start(scrapPage) {
+  if (scrapPage.includes("www.ine.es")) {
+    formmater.formatData(await ineScraper.scrap())
+  }
+  if (scrapPage.includes("www.motor.es")) {
+  formmater.formatData(fuelScraper.scrap())
+  }
 }
- start()
+ start(process.env.PAGES_TO_SCRAPE)
 
 //format and send  data
 
